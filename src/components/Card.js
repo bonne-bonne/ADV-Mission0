9d171ea6a1f18906198f1f2ce4136a4ea2c207de
Bonnie Wall <bm-wall@outlook.com>
2022-11-16 22:12:08 +1300

    finished most of layout, next to work on responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Card.module.css";
 import hongKong4 from "../assets/HK4.jpg";
+import paint from "../assets/streak.png";
+import paint2 from "../assets/streak2.png";
 
 export default function Card(props) {
   return (
@@ -12,12 +14,13 @@ export default function Card(props) {
         <div className={styles.card_color}>
           {/* <div className={styles.shadow}></div> */}
           <div className={styles.card_heading_container}>
-          <div className={styles.line}></div>
+          <img src={props.paint} className={styles.paint} />
+          {/* <div className={styles.line}></div> */}
             <p className={styles.card_heading}>{props.heading}</p>
-            <div className={`${styles.line} ${styles.line_right}`}></div>
+            {/* <div className={`${styles.line} ${styles.line_right}`}></div> */}
             
           </div>
-          <div className={styles.card_body_text}>Come here to eat good food. Tsim stniusdbuiusdf b . hihoisdf hiosdofoihsdf hoisdoihfosd hosdifosdhshoihsdofihiosidhfosihdfsdfsf sddfsfs jnsfj jsdfb niosdnbfin o</div>
+          <div className={styles.card_body_text}>{props.bodyText}</div>
         </div>
       </div>
     </div>
