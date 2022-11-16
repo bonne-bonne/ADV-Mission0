9c35f3992b7753d9e3984be39e54f63c8698118d
Bonnie Wall <bm-wall@outlook.com>
2022-11-16 23:53:14 +1300

    cleaned up header file and css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,40 @@
 import React from "react";
 import styles from "./Header.module.css";
-import hongKong from "../assets/HK.jpg";
+
+// ========== IMAGE IMPORTS ========== //
+// import hongKong from "../assets/HK.jpg";
+// import hongKong3 from "../assets/HK3.jpg";
+// import hongKong4 from "../assets/HK4.jpg";
 import hongKong2 from "../assets/HK2.jpg";
-import hongKong3 from "../assets/HK3.jpg";
-import hongKong4 from "../assets/HK4.jpg";
-import Searchbar from "./Searchbar";
+
+
 
 export default function Header() {
   return (
     <div className={styles.header_outer_container}>
      
       <div className={styles.background_img}>
-        {/* <img src={hongKong} /> */}
         <img src={hongKong2} />
-        {/* <img src={hongKong3} /> */}
-        {/* <img src={hongKong4} /> */}
       </div>
       <div className={styles.background_img_tone}></div>
 
-      <div className={styles.container}>
-        <h1 className={styles.heading}>Hop over to <span>Hong Kong!</span> </h1>
 
-        {/* <Searchbar /> */}
+      <div className={styles.container}>
+        
+        <h1 className={styles.heading}>Hop over to <span>Hong Kong!</span> </h1>
 
         <div className={styles.search_bar_container}>
           <input
             className={styles.input_field}
             type="text"
-            placeholder="What would you like to learn about?..."
+            placeholder="Explore what Hong Kong has to offer..."
           ></input>
           <div className={styles.search_btn}>SEARCH</div>
         </div>
 
         
       </div>
+
 
     </div>
   );
