dbf4c9883a74125136ed9dcedcb8084be3900a0a
Bonnie Wall <bm-wall@outlook.com>
2022-11-17 16:37:12 +1300

    added wheel animation but not happy with it

--- src/components/Footer_animation.js
+++ src/components/Footer_animation.js
@@ -4,12 +4,17 @@ import junk from "../assets/junk.png";
 import junkPink from "../assets/junk_pink.png";
 import junkWhite from "../assets/junk_white.png";
 import hills from "../assets/hills.png";
+import skylinePink from "../assets/skylinePink.png";
+import wheel from "../assets/wheel.png";
+import stand from "../assets/stand.png";
 
 export default function Footer_animation() {
   return (
     <div className={styles.footer_animation_outer_container}>
         <div className={`${styles.test} ${styles.moving}`}><img src={junkPink} className={styles.junk} /></div>
         <div className={styles.test}><img src={hills} className={styles.hills} /></div>
+        <div className={styles.test}><img src={skylinePink} className={styles.skyline} /></div>
+        {/* <div className={`${styles.test} ${styles.wheelset}`}><img src={wheel} className={styles.wheel} /><img src={stand} className={styles.stand} /></div> */}
     
         {/* <div className={styles.test}></div> */}
         {/* <div className={styles.test}></div> */}
