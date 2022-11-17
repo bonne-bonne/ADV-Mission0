8f7ab0a4e1c61c0516ef46dcc1161d0050fa32c8
Bonnie Wall <bm-wall@outlook.com>
2022-11-17 13:36:07 +1300

    added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Navbar.module.css'
 import { useState } from 'react'
 
+import building from '../assets/building.png'
+
 export default function Navbar() {
   const [isHidden, setIsHidden] = useState(true);
 
@@ -18,8 +20,9 @@ export default function Navbar() {
 
       <div className={styles.nav_content_container}>
 
-        <div className={styles.logo_container}>HIDDEN HONG KONG</div>
-        <div className={styles.logo_container_sm}>HHK</div>
+
+        <div className={styles.logo_container}><div><img src={building} className={styles.logo} /></div>HIDDEN HONG KONG</div>
+        <div className={styles.logo_container_sm}><div><img src={building} className={styles.logo} /></div>HHK</div>
         
         <div className={styles.hamburger_menu} onClick={toggleMenu}>
           <span className={styles.bar}></span>
